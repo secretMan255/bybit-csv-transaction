17fb862594425f9efd6e880be32a91dbf19474d2
Yap Yi Liang <yapyiliangww321@gmail.com>
2026-01-11 19:59:55 +0800

    fixed : layout min height

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,9 +4,9 @@ interface Props {
 
 export default function Layout({ children }: Props) {
   return (
-    <div className="flex h-screen bg-slate-50 overflow-hidden">
-      <div className="flex-1 flex flex-col h-full overflow-hidden">
-        <main className="flex-1 overflow-auto p-4 lg:p-5">
+    <div className="min-h-dvh bg-slate-50 flex overflow-hidden">
+      <div className="flex-1 flex flex-col min-h-0 overflow-hidden">
+        <main className="flex-1 min-h-0 overflow-auto p-4 lg:p-5">
           <div className="w-full space-y-6">{children}</div>
         </main>
       </div>
